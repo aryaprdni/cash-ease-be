7858802374aa40e6d7415a9aca0a61b2c16dfc9e
aryaprdni <aryaperdanairawan473@gmail.com>
2025-06-16 16:24:15 +0700

    feat: add .env file for database connection and update CORS settings

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['*'],
+    origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
